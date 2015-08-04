6848498ba225d58729ab7289877d26418151bc5a
diskerror <reidw@rawsound.com>
2015-08-03 17:49:40 -0700

    fixed formatting 	modified:   TypedAbstract.php

--- TypedAbstract.php
+++ TypedAbstract.php
@@ -192,50 +192,50 @@ abstract class TypedAbstract implements Countable
 		}
 
 		//	Pretty print from Zend/Json/Json.php v2.4.2.
-        $tokens = preg_split('|([\{\}\]\[,])|', $j, -1, PREG_SPLIT_DELIM_CAPTURE);
-        $result = "";
-        $indent = 0;
+		$tokens = preg_split('|([\{\}\]\[,])|', $j, -1, PREG_SPLIT_DELIM_CAPTURE);
+		$result = "";
+		$indent = 0;
 
-        $inLiteral = false;
-        foreach ($tokens as $token) {
-            $token = trim($token);
-            if ($token == "") {
-                continue;
-            }
+		$inLiteral = false;
+		foreach ($tokens as $token) {
+			$token = trim($token);
+			if ($token == "") {
+				continue;
+			}
 
-            if (preg_match('/^("(?:.*)"):[ ]?(.*)$/', $token, $matches)) {
-                $token = $matches[1] . ': ' . $matches[2];
-            }
+			if (preg_match('/^("(?:.*)"):[ ]?(.*)$/', $token, $matches)) {
+				$token = $matches[1] . ': ' . $matches[2];
+			}
 
-            $prefix = str_repeat("\t", $indent);
-            if (!$inLiteral && ($token == '{' || $token == '[')) {
-                $indent++;
-                if ($result != '' && $result[strlen($result)-1] == "\n") {
-                    $result .= $prefix;
-                }
-                $result .= "$token\n";
-            }
-            elseif (!$inLiteral && ($token == '}' || $token == ']')) {
-                $indent--;
-                $prefix = str_repeat("\t", $indent);
-                $result .= "\n$prefix$token";
-            }
-            elseif (!$inLiteral && $token == ',') {
-                $result .= "$token\n";
-            }
-            else {
-                $result .= ($inLiteral ?  '' : $prefix) . $token;
+			$prefix = str_repeat("\t", $indent);
+			if (!$inLiteral && ($token == '{' || $token == '[')) {
+				$indent++;
+				if ($result != '' && $result[strlen($result)-1] == "\n") {
+					$result .= $prefix;
+				}
+				$result .= "$token\n";
+			}
+			elseif (!$inLiteral && ($token == '}' || $token == ']')) {
+				$indent--;
+				$prefix = str_repeat("\t", $indent);
+				$result .= "\n$prefix$token";
+			}
+			elseif (!$inLiteral && $token == ',') {
+				$result .= "$token\n";
+			}
+			else {
+				$result .= ($inLiteral ?  '' : $prefix) . $token;
 
-                //remove escaped backslash sequences causing false positives in next check
-                $token = str_replace('\\', '', $token);
-                // Count # of unescaped double-quotes in token, subtract # of
-                // escaped double-quotes and if the result is odd then we are
-                // inside a string literal
-                if ((substr_count($token, '"')-substr_count($token, '\\"')) % 2 != 0) {
-                    $inLiteral = !$inLiteral;
-                }
-            }
-        }
+				//remove escaped backslash sequences causing false positives in next check
+				$token = str_replace('\\', '', $token);
+				// Count # of unescaped double-quotes in token, subtract # of
+				// escaped double-quotes and if the result is odd then we are
+				// inside a string literal
+				if ((substr_count($token, '"')-substr_count($token, '\\"')) % 2 != 0) {
+					$inLiteral = !$inLiteral;
+				}
+			}
+		}
 		return $result . "\n";
 	}
 
