d15b490419c4ed1c5659c59e0c4f5f69cc701d9e
Reid Woodbury Jr <reidw@rawsound.com>
2018-11-05 11:16:16 -0800

    SAAnything new really allows anything

--- src/SAAnything.php
+++ src/SAAnything.php
@@ -22,44 +22,11 @@ class SAAnything extends ScalarAbstract
 {
 	public function set($in)
 	{
-		switch (gettype($in)) {
-			case 'object':
-				if ($in instanceof ScalarAbstract) {
-					$this->_value = $in->get();
-					break;
-				}
-				if ($in instanceof TypedInterface) {
-					throw new UnexpectedValueException('you must be kidding');
-				}
-				if (method_exists($in, '__toString')) {
-					$this->_value = $in->__toString();
-					break;
-				}
-				if (method_exists($in, 'format')) {
-					$this->_value = $in->format('c');
-					break;
-				}
-
-				if (method_exists($in, 'toArray')) {
-					$in = $in->toArray();
-				}
-			//	other objects fall through, object to array falls through
-			case 'array':
-				$jsonStr     = json_encode($in);
-				$jsonLastErr = json_last_error();
-				if ($jsonLastErr !== JSON_ERROR_NONE) {
-					throw new UnexpectedValueException(json_last_error_msg(), $jsonLastErr);
-				}
-				$this->_value = $jsonStr;
-			break;
-
-			case 'null':
-			case 'NULL':
-				$this->_setNullOrDefault();
-			break;
-
-			default:
-				$this->_value = $in;
+		if (is_null($in)) {
+			$this->_setNullOrDefault();
+		}
+		else {
+			$this->_value = $in;
 		}
 	}
 }
