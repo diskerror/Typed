320ab70b5c127151e789c7d2068002e41375d4e9
Reid Woodbury Jr <reid@rawsound.com>
2021-06-08 14:11:43 -0700

    move jsonPrint out of namespace 	modified:   src/TypedAbstract.php

--- src/TypedAbstract.php
+++ src/TypedAbstract.php
@@ -7,6 +7,11 @@
  * @license        http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
  */
 
+function jsonPrint($in)
+{
+	fwrite(STDERR, json_encode($in, JSON_PRETTY_PRINT) . PHP_EOL);
+}
+
 namespace Diskerror\Typed;
 
 use Countable;
@@ -14,11 +19,6 @@ use IteratorAggregate;
 use Serializable;
 use JsonSerializable;
 use const PHP_EOL;
-
-function jsonPrint($in)
-{
-	fwrite(STDERR, json_encode($in, JSON_PRETTY_PRINT) . PHP_EOL);
-}
 
 /**
  * Class TypedInterface
